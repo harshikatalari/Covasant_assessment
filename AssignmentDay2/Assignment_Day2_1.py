e61a530d7e7f4b47a5b7fac062026951472b66de
harshikatalari <142504651+harshikatalari@users.noreply.github.com>
2025-04-11 19:14:09 +0530

    Update Assignment_Day2_1.py

--- AssignmentDay2/Assignment_Day2_1.py
+++ AssignmentDay2/Assignment_Day2_1.py
@@ -4,18 +4,18 @@
 import glob
 import os.path
 
-path=r""
+path=r""                  #specify the directory
 maxfiles={}
 
-def recursive(path):
-    files=glob.glob(os.path.join(path,"*"))
+def recursive(path):                      
+    files=glob.glob(os.path.join(path,"*"))            #to find all the individual files in a directory
     for f in files:
-        if os.path.isdir(f):
+        if os.path.isdir(f):                #if directory then recursive call
             recursive(f)
         elif os.path.isfile(f):
-            maxfiles[os.path.basename(f)]=os.path.getsize(f)
+            maxfiles[os.path.basename(f)]=os.path.getsize(f)              #adding filename as key and path of file a directory {}
 recursive(path)
-maxsize=sorted(maxfiles.values())[-1]
+maxsize=sorted(maxfiles.values())[-1]     #sorting file size default ascending and gives file which has max size
 for f in maxfiles:
     size=maxfiles[f]
     if size==maxsize:
