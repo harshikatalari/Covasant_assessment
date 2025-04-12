fd17d9a2bf4a11309ebe5a195e2a24121be217b6
harshikatalari <142504651+harshikatalari@users.noreply.github.com>
2025-04-12 10:13:59 +0530

    Update Assignment_Day2_2.py

--- AssignmentDay2/Assignment_Day2_2.py
+++ AssignmentDay2/Assignment_Day2_2.py
@@ -11,17 +11,17 @@ folder=glob.glob(os.path.join(path,"*"))
 data=r""
 
 def recursive(folder,data):
-    for file in folder:
-        if os.path.isfile(file):
-            if file.endswith(".txt"):
-                with open(file,"rt") as f:
+    for file in folder:          #folder is list of all the paths in a given directory obtained by glob.glob method
+        if os.path.isfile(file):     #if file is a file 
+            if file.endswith(".txt"):     #and ends with .txt extension
+                with open(file,"rt") as f:      #then reading the file
                     lines=f.readlines()
-                with open(data,"at") as ft:
-                    ft.write("\n******************************\n")
-                    ft.write("Data from file"+file+"\n")
-                    ft.writelines(lines)
+                with open(data,"at") as ft:        #data is the path of a file given by user where we want to append the textlines 
+                    ft.write("\n******************************\n")     #writing the data
+                    ft.write("Data from file"+file+"\n")          #writing the data read from the .txt extension file which is 'lines'
+                    ft.writelines(lines)       
                     ft.write("\n******************************\n")
         else:
-            sub=glob.glob(os.path.join(file,"*"))
+            sub=glob.glob(os.path.join(file,"*"))        #if the file is not a file but a directory recursive call
             recursive(sub,data)
 recursive(folder,data)
