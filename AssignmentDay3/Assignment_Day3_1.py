fb83da81e25465bc656b66ae207bbc6c60d7bccc
harshikatalari <142504651+harshikatalari@users.noreply.github.com>
2025-04-12 10:07:50 +0530

    Update Assignment_Day3_1.py

--- AssignmentDay3/Assignment_Day3_1.py
+++ AssignmentDay3/Assignment_Day3_1.py
@@ -9,16 +9,16 @@ input = [[[ '(0,1,2)' , '(3,4,5)'], ['(5,6,7)' , '(9,4,2)']]]
 output=[]
 
 def flatten(input):
-    first=[]
+    first=[]             #outer first list
     for x in input:
-        sec=[]
+        sec=[]                #inner second list
         for y in x:
             for z in y:
-                s=(z.strip("()").split(","))
-                num=[int(t) for t in s]
-                sec.append(num)
-        first.append(sec)
-    output.append(first)
+                s=(z.strip("()").split(","))      #strips the tuple and splits each element and returns in the form of list
+                num=[int(t) for t in s]      #as the elements in the returned list are 'str' converting them into 'int'
+                sec.append(num)         
+        first.append(sec)         #adding elements to second list
+    output.append(first)             #adding second list to first list
 flatten(input)
 print(output)
 
