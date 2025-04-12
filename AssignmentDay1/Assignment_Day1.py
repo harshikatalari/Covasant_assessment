a0ee07e48c0594e080f1e3af06101d3f6cd98965
harshikatalari <142504651+harshikatalari@users.noreply.github.com>
2025-04-12 09:57:37 +0530

    Update Assignment_Day1.py

--- AssignmentDay1/Assignment_Day1.py
+++ AssignmentDay1/Assignment_Day1.py
@@ -14,20 +14,20 @@
 D1={'ok':1,'nok':2}
 D2={'ok':2,'new':3}
 
-D_UNION={**D1,**D2}      
+D_UNION={**D1,**D2}      #union of keys
 print(D_UNION)
 
-D_INTERSECTION={}
+D_INTERSECTION={}       #intersection of keys          
 for k in D1.keys()&D2.keys():
     D_INTERSECTION[k]=D1[k]
 print(D_INTERSECTION)
 
 D_DIFFERENCE={}
-for k in D1.keys()-D2.keys():
+for k in D1.keys()-D2.keys():   #difference of keys
     D_DIFFERENCE[k]=D1[k]
 print(D_DIFFERENCE)
 
-D_MERGE={}
+D_MERGE={}           #merging of all the keys       
 for k in D1.keys()|D2.keys():
     D_MERGE[k]=D1.get(k,0)+D2.get(k,0)
 print(D_MERGE)
