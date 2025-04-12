fc31e61f4025d345852d0cac4c3ca8e914eac667
harshikatalari <142504651+harshikatalari@users.noreply.github.com>
2025-04-12 10:09:32 +0530

    Update Assignment_Day3_2.py

--- AssignmentDay3/Assignment_Day3_2.py
+++ AssignmentDay3/Assignment_Day3_2.py
@@ -7,11 +7,11 @@
 input = [1,2,3, [1,2,3,[3,4],2]]
 output=[]
 
-def f_ip(input):
-    for f in input:
-        if isinstance(f,list):
+def f_ip(input): 
+    for f in input:               #iteration of given list
+        if isinstance(f,list):      #if the element f is list recursive call
             f_ip(f)
         else:
-            output.append(f)
+            output.append(f)       #if the element is not a list then appending it to list named 'output'
 f_ip(input)
 print(output)
