bea85d3b36d50a51a20583b4c406641b4635a22c
harshikatalari <142504651+harshikatalari@users.noreply.github.com>
2025-04-15 22:07:47 +0530

    Update Assignment_Day5.py

--- AssignmentDay5/Assignment_Day5.py
+++ AssignmentDay5/Assignment_Day5.py
@@ -6,12 +6,12 @@ import os
 
 def download_url(url):
     try:
-        response = requests.get(url, timeout=10)
-        path = urlparse(url).path
-        filename = os.path.basename(path) or "index.html"
+        response = requests.get(url, timeout=10)                # Sending an HTTP GET request with a 10-second timeout
+        path = urlparse(url).path                               # Extracting the path part from the URL
+        filename = os.path.basename(path) or "index.html"       # Gets the filename from the path or will use "index.html" if its empty
         filename = filename.replace("/", "_")  
 
-        with open(filename, "wb") as f:  # we use 'wb' here for binary content
+        with open(filename, "wb") as f:  # Opens the file in binary write mode
             f.write(response.content)
         print(f"Downloaded: {url}")
     except Exception as e:
@@ -19,12 +19,12 @@ def download_url(url):
 
 def get_all_links(base_url):
     try:
-        response = requests.get(base_url)    
+        response = requests.get(base_url)                       # Sends a GET request to the base URL
         soup = BeautifulSoup(response.text, 'html.parser')
         links = []
         for a_tag in soup.find_all('a', href=True):
-            full_url = urljoin(base_url, a_tag['href']) #joining baseurl with hreference
-            if full_url.startswith("http"):   #if file ends with http appending link to links list []
+            full_url = urljoin(base_url, a_tag['href'])         #making full file path to save content
+            if full_url.startswith("http"):                     #if file ends with http appending link to links list []
                 links.append(full_url)
         return links
     except Exception as e:
@@ -32,14 +32,17 @@ def get_all_links(base_url):
         return []
 
 def download_all_links(base_url):
-    print(f"Parsing and downloading links from: {base_url}") #here baseurl has many sub urls
-    links = get_all_links(base_url)           #links is list of all the links in baseurl
-    print(f"Found {len(links)} links.")   #returns length of links found in given base_url
+    print(f"Parsing and downloading links from: {base_url}") 
+    links = get_all_links(base_url)                             #links is list of all the links in baseurl
+    print(f"Found {len(links)} links.")                         #returns length of links found in given base_url
 
-    with ThreadPoolExecutor(max_workers=5) as executor:
-        executor.map(download_url, links)
+    with ThreadPoolExecutor(max_workers=5) as executor:         # Creates a thread pool with 5 worker threads
+        executor.map(download_url, links)                       # Distributes the downloading task across threads
 
 
 if __name__ == '__main__':
-    base_url = "https://www.tpointtech.com/python-tutorial" #we can use any url here
+    base_url = "https://www.tpointtech.com/python-tutorial"      #we can use any url here
     download_all_links(base_url)
+
+    
+     
